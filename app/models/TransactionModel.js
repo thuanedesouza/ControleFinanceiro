9cc834e9f94d1f964f1c3ee89ad4f7686110a73b
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2020-09-11 11:26:09 -0300

    campos obrigatórios

--- app/models/TransactionModel.js
+++ app/models/TransactionModel.js
@@ -1,15 +1,43 @@
 const mongoose = require('mongoose');
 
 let schema = mongoose.Schema({
-  description: String,
-  value: Number,
-  category: String,
-  year: Number,
-  month: Number,
-  day: Number,
-  yearMonth: String,
-  yearMonthDay: String,
-  type: String,
+  description: {
+    type: String,
+    require: true
+  },
+  value: {
+    type: Number,
+    require: true
+  },
+  category: {
+    type: String,
+    require: true
+  },
+  year: {
+    type: Number,
+    require: true
+  },
+  month: {
+    type: Number,
+    require: true
+  },
+  day: {
+    type: Number,
+    require: true
+  },
+  yearMonth: {
+    type: String,
+    require: true
+  },
+  yearMonthDay: {
+    type: String,
+    require: true
+  },
+  type:
+  {
+    type: String,
+    require: true
+  }
 });
 
 const TransactionModel = mongoose.model('transaction', schema);
