64562365497f59d0bc5b439df9ae87a2f5d56a63
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2020-09-13 07:30:22 -0300

    testint api

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as api from './apiService'
+import Spinner from './components/Spinner';
+
 
 export default function App() {
-  const testAPI = async () => {
-    const result = await api.getAllTransacitons(`2019-10`);
-    console.log(result);
-  }
-  testAPI();
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  const [allTransactions, setAllTransactions] = useState([]);
+
+  useEffect(() => {
+    const getTransactions = async () => {
+      const transactions = await api.getAllTransacitons(`2019-10`);
+
+      setTimeout(() => {
+        setAllTransactions(transactions)
+      }, 2000);
+    };
+    getTransactions();
+  }, []);
+
+
+  return (<div>
+    <h1>Desafio Final do Bootcamp Full Stack</h1>
+    {allTransactions.length === 0 && < Spinner />}
+  </div>
+
+  );
 }
+
--- app/client/src/apiService.js
+++ app/client/src/apiService.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
-const API_URL = 'http://localhost:3001/api/transactions/'
+const API_URL = `http://localhost:3001/api/transactions`
 
 async function getAllTransacitons(yearMonth) {
   //pegando resultado da promisse que vem da api
   const res = await axios.get(`${API_URL}/${yearMonth}`);
-  const transactions = JSON.parse(await res.transactions);
+  const transactions = JSON.parse(await res.data.transactions);
   //console.log(transactions)
   //const { description } = transactions;
-
+  console.log(transactions)
   return {
     // ...
     transactions
@@ -23,7 +23,7 @@ async function updateTransaction(transaction) {
 
 
 async function deleteTransaction(transaction) {
-  const response = await axios.delete(`${API_URL}/${transaction.id}`);
+  const response = await axios.delete(`${API_URL} / ${transaction.id}`);
   return response.data;
 }
 
